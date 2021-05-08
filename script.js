ff5c9de1897cb382b71bdd27bf222b3aff1bda40
Tuhingh2632 <75439494+Tuhingh2632@users.noreply.github.com>
2021-05-08 11:26:26 +0530

    Adding Animations

---
+++ script.js
@@ -0,0 +1,53 @@
+var progressBars = document.querySelectorAll('.Skill-progress > div');
+var skillsContainer = document.getElementById('skills-container');
+window.addEventListener('scroll', checkScroll);
+var animationDone = false;
+
+
+
+function initialiseBars() {
+    for (var bar of progressBars) {
+        bar.style.width = 0 + '%';
+    }
+}
+
+initialiseBars();
+
+
+
+function fillBars() {
+
+    for (let bar of progressBars) {
+        let currentWidth = 0;
+        let interval = setInterval(function () {
+            let targetWidth = bar.getAttribute('data-bar-width');
+            if (currentWidth >= targetWidth) {
+                clearInterval(interval);
+                return;
+            }
+            currentWidth++;
+            bar.style.width = currentWidth + '%';
+        }, 5);
+    }
+}
+
+
+
+function checkScroll() {
+
+    var coordinates = skillsContainer.getBoundingClientRect();
+    if (!animationDone && coordinates.top <= window.innerHeight) {
+        animationDone = true;
+        fillBars();
+    } else if (coordinates.top > window.innerHeight) {
+        animationDone = false;
+        initialiseBars();
+    }
+}
+
+
+
+
+
+// This event fills the progress bars if they are displayed on the screen when the page is loaded.
+//window.addEventListener("load", checkScroll);
